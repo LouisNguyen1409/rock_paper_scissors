658cfe4467ec2ae9807c635abbea9464e2da6147
Louis Nguyen <ndmn004@gmail.com>
2023-06-02 17:33:07 +1000

    Add initial files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# rock_paper_scissors
+# This is 4/100 projects that I do
+This project is a simple python project for playing rock, paper, scissors
---
+++ rock_paper_scissors.py
@@ -0,0 +1,58 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if user_input == 0:
+    print(rock)
+elif user_input == 1:
+    print(paper)
+else:
+    print(scissors)
+
+computer_choice = random.randint(0, 2)
+
+print("Computer chose: ")
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+if user_input == computer_choice:
+    print("Draw.")
+elif user_input == 0 and computer_choice == 2:
+    print("You win.")
+elif computer_choice == 0 and user_input == 2:
+    print("You lose.")
+elif user_input < computer_choice:
+    print("You lose.")
+elif user_input > computer_choice:
+    print("You win.")
